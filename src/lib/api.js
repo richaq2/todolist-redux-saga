e2f149e339825048126a9b7e09222b6725ec3a2d
richaq2 <65802201+richaq2@users.noreply.github.com>
2022-07-08 19:37:13 +0530

    Delete src/lib directory

--- src/lib/api.js
+++
@@ -1,9 +0,0 @@
-const BASE_URL = "http://localhost:4000/todos";
-export function Todo {
-    id:Number;
-    title:String;
-    description:String;
-    start_date:Date;
-    end_date:Date;
-    assignes:String;
-}
